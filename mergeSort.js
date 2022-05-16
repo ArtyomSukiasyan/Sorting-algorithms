23ed7722adbfac232daad8cbfc1cfb8cc564ce5c
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2022-05-16 20:39:35 +0400

    feat: add mergeSort.js

---
+++ mergeSort.js
@@ -0,0 +1,26 @@
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const split = Math.floor(arr.length / 2);
+  let left = arr.slice(0, split);
+  let right = arr.slice(split);
+
+  left = mergeSort(left);
+  right = mergeSort(right);
+
+  const sorted = [];
+
+  while (left.length > 0 || right.length > 0) {
+    if (right.length === 0 || left[0] <= right[0]) {
+      sorted.push(left.shift());
+    } else {
+      sorted.push(right.shift());
+    }
+  }
+
+  return sorted;
+}
+
+console.log(mergeSort([1, 5, 4, 2, 5, 8, 7, 3, 65, 2]));
